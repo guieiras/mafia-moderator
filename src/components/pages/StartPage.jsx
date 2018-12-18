2853d4821a07e2e59af6e93c6084ed32c8acd40a
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-12-18 09:20:24 -0200

    Added new player handling

--- src/components/pages/StartPage.jsx
+++ src/components/pages/StartPage.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import { Navbar, Page, BlockTitle, List, ListItem } from 'framework7-react';
+import {
+  BlockTitle,
+  Button,
+  Icon,
+  List,
+  ListInput,
+  ListItem,
+  Navbar,
+  Page,
+} from 'framework7-react';
+import uuid from 'uuid/v1';
 
 export default class StartPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      players: [],
-      selectedPlayers: {}
-    }
+    this.state = { currentPlayer: '', players: [], selectedPlayers: {} };
+    this.handleInput = this.handleInput.bind(this);
+    this.addPlayer = this.addPlayer.bind(this);
+  }
+
+  addPlayer() {
+    const { currentPlayer, players } = this.state;
+    players.push({ id: uuid(), name: currentPlayer })
+    this.setState({ players });
+  }
+
+  handleInput(event) {
+    this.setState({ currentPlayer: event.target.value });
   }
 
   render() {
@@ -20,6 +39,11 @@ export default class StartPage extends Component {
             <ListItem checkbox key={player.id} checked={this.state.selectedPlayers[player.id]} title={player.name}>
             </ListItem>)
         }
+        <ListInput type="text" placeholder="Novo Jogador" inputStyle={{width: '90%'}} onInput={this.handleInput}>
+          <Button fill color="green" slot="inner-end" className="player-button" onClick={this.addPlayer}>
+            <Icon f7="add"></Icon>
+          </Button>
+        </ListInput>
       </List>
     </Page>
   }
