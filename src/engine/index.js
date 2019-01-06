c651dc14da3285fe6edde066ceb883944af35b3a
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-01-06 17:03:47 -0200

    Added engine construction

--- src/engine/index.js
+++ src/engine/index.js
@@ -1,11 +1,13 @@
 import { observable } from "mobx";
 import Clock from "./clock";
 import Stack from "./stack";
+import Role from "./role";
+import db from "../boundaries/database";
+import Player from "./player";
+import roles from "../roles";
 
 export default class Engine {
   constructor(game) {
-    this.players = game.players;
-    this.roles = game.roles;
     this.stack = new Stack();
     this.clock = new Clock();
     this.state = observable({
@@ -14,6 +16,13 @@ export default class Engine {
       roles: this.roles,
       events: [],
       stack: this.stack.state,
+    });
+    this.state.roles = Object.keys(game.roles).map((roleId) => new Role(roleId, game.roles[roleId]));
+    db.players.toArray().then((dbPlayers) => {
+      this.state.players = game.players.map((playerId) => new Player(
+        dbPlayers.filter((dbPlayer) => dbPlayer.id == playerId)[0]
+      ));
+      this.players = [];
     });
   }
 }
