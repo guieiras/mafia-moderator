7d4db39164500f89060944b77b2fba5380d6691e
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-01-20 23:12:24 -0200

    Fixed wakeup translator

--- src/engine/translators/wakeup.js
+++ src/engine/translators/wakeup.js
@@ -1,13 +1,13 @@
 import I18n from '../../i18n';
 
 export default ({
-  MafiaKilled(player) {
-    return I18n.reports.mafiaKilled.replace('$player', player.name);
+  BittenByVampire(player) {
+    return I18n.reports.bittenByVampire.replace('$player', player.name);
   },
   Kidnapped(player) {
     return I18n.reports.kidnapped.replace('$player', player.name);
   },
-  BittenByVampire(player) {
-    return I18n.reports.bittenByVampire.replace('$player', player.name);
-  }
+  MafiaKilled(player) {
+    return I18n.reports.mafiaKilled.replace('$player', player.name);
+  },
 });
